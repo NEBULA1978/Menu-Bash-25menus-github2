77b9682d766df20eb6c67f8b3ce5e89908450cbc
NEBULA1978 <rpascualey@gmail.com>
2023-04-01 22:02:16 +0200

    Simplificando codigo antiguo al nuevo

---
+++ menubase-nuevo2.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Definimos un array con las opciones del menú. Cada opción es una cadena con dos partes separadas por el carácter '|': el nombre de la opción y el comando correspondiente.
+opciones=(
+    "Mostrar directorio:|ls"
+
+    "Mostrar calendario:|cal"
+
+    "Mostrar fecha de hoy:|date"
+
+    "Mostrar archivos dentro de las carpetas de los menus:|bash ./miscript-ver-carpetas.sh | more"
+
+    "Ejecutar menubase.sh de la carpeta AUTOMATIZAR-TAREAS_BASH_MENU-main:|cd AUTOMATIZAR-TAREAS_BASH_MENU-main && bash ./menubase.sh && cd .."
+
+    "Ejecutar menubase.sh de la carpeta CREAR-USUARIOS-MENU-main:|cd CREAR-USUARIOS-MENU-main && bash ./menubase.sh && cd .."
+
+    "Ejecutar menubase.sh de la carpeta crear-ver-archivoss-bash-main:|cd crear-ver-archivoss-bash-main && bash ./menubase.sh && cd .."
+
+    "Ejecutar menubase.sh de la carpeta grep-awk-bash-main:|cd grep-awk-bash-main && bash ./menubase.sh && cd .."
+
+    "Ejecutar menubase2.sh de la carpeta grep-awk-bash-main:|cd grep-awk-bash-main && bash ./menubase2.sh && cd .."
+
+    "Ejecutar menubase-sed.sh de la carpeta grep-awk-bash-main:|cd grep-awk-bash-main && bash ./menubase-sed.sh && cd .."
+
+    "Ejecutar menubase.sh de la carpeta Master-bash-Javascript-main:|cd Master-bash-Javascript-main && bash ./menubase.sh && cd .."
+
+    "Ejecutar menubase.sh de la carpeta menu-bash-a-python-master-main:|cd menu-bash-a-python-master-main && bash ./menubase.sh && cd .."
+
+    "Ejecutar menubase.sh de la carpeta Menu-bash-awk-main:|cd Menu-bash-awk-main && bash ./menubase.sh && cd .."
+
+    "Ejecutar menubase.sh de la carpeta menu-bash-javascript-master-main:|cd menu-bash-javascript-master-main && bash ./menubase.sh && cd .."
+
+    "Ejecutar menubase.sh de la carpeta menu-bash-javascript-prueba1-main:|cd menu-bash-javascript-prueba1-main && bash ./menubasejavascript.sh && cd .."
+
+    "Ejecutar menubase.sh de la carpeta Menu-bash-mastermind-js-master-main:|cd Menu-bash-mastermind-js-master-main && bash ./menubase.sh && cd .."
+
+    "Ejecutar menubase1.sh de la carpeta Menu-bash-mastermind-js-master-main:|cd Menu-bash-mastermind-js-master-main && bash ./menubase1.sh && cd .."
+
+    "Ejecutar menubase1.sh de la carpeta menu-bash-menu-python-main:|cd menu-bash-menu-python-main && bash ./menubase.sh && cd .."
+
+    "Ejecutar menubase1.sh de la carpeta Menu-bash-sarosi-main:|cd Menu-bash-sarosi-main && bash ./menubase1.sh && cd .."
+)
+
+# Definimos la función que muestra el menú
+function mostrar_menu {
+    echo "---------------------"
+    echo " MENÚ PRINCIPAL "
+    echo "---------------------"
+    for opcion in "${!opciones[@]}"; do
+        nombre_opcion=$(echo "${opciones[opcion]}" | cut -d '|' -f 1)
+        echo " $opcion) $nombre_opcion"
+    done
+    echo " m) Menú secundario"
+    echo " q) Salir"
+}
+
+# Definimos la función que ejecuta la opción seleccionada
+function ejecutar_opcion {
+    opcion_seleccionada=$1
+    comando=$(echo "${opciones[opcion_seleccionada]}" | cut -d '|' -f 2)
+    eval $comando
+}
+
+# Bucle principal
+while true; do
+    mostrar_menu
+    read -p "Seleccione una opción: " opcion
+    if [[ $opcion == "q" ]]; then
+        break
+    elif [[ $opcion == "m" ]]; then
+        bash ./menu_secundario.sh
+    elif [[ $opcion -ge 0 && $opcion -lt ${#opciones[@]} ]]; then
+        ejecutar_opcion $opcion
+        read -p "Presione cualquier tecla para continuar..."
+    else
+        echo "Opción inválida. Presione cualquier tecla para continuar..."
+        read -n 1
+    fi
+done
+
+# El código es un script de bash que define un menú con varias opciones, cada una con un comando asociado. Las opciones incluyen comandos para mostrar el directorio actual, el calendario o la fecha actual, así como ejecutar scripts en diferentes carpetas.
+
+# El menú es presentado al usuario en un bucle principal, donde se muestra cada opción y se espera la selección del usuario. Si la selección es válida, se ejecuta el comando asociado. Si la selección es "q", el bucle termina y el script finaliza.
+
+# El código utiliza técnicas comunes de bash, como la definición de variables, el uso de arrays y la manipulación de cadenas para extraer los comandos asociados a cada opción. También se utilizan construcciones condicionales para validar la selección del usuario y para salir del bucle cuando se selecciona la opción "q".
